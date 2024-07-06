c7a8eeb9b94e28e43b85ceab8b662e8f5d323316
Harsh  Salve <119628541+harsh4677@users.noreply.github.com>
2024-07-06 18:09:31 +0530

    Time: 70 ms (24.79%), Space: 50.1 MB (53.58%) - LeetHub

---
+++ 2741-function-composition/2741-function-composition.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+
+    return function(x) {
+        if (functions.length === 0) {
+            return x;
+        }
+        return functions.reduceRight((accumulator, fn) => fn(accumulator), x);
+    };
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
