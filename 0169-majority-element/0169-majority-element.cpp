8b631de9d50d219d558c71f0271faf3bcaf2a12c
Harsh  Salve <119628541+harsh4677@users.noreply.github.com>
2024-07-29 08:52:17 +0530

    Time: 23 ms (21.82%), Space: 27.2 MB (37.71%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
     int majorityElement(vector<int>& v) {
-        int n = v.size();
-        int cnt = 0; // count
-        int el; // Element
+       int n = v.size();
 
-        //applying the algorithm:
-        for (int i = 0; i < n; i++) {
-            if (cnt == 0) {
-                cnt = 1;
-                el = v[i];
-            }
-            else if (el == v[i]) cnt++;
-            else cnt--;
+    //declaring a map:
+    map<int, int> mpp;
+
+    //storing the elements with its occurnce:
+    for (int i = 0; i < n; i++) {
+        mpp[v[i]]++;
+    }
+
+    //searching for the majority element:
+    for (auto it : mpp) {
+        if (it.second > (n / 2)) {
+            return it.first;
         }
+    }
 
-        //checking if the stored element
-        // is the majority element:
-        int cnt1 = 0;
-        for (int i = 0; i < n; i++) {
-            if (v[i] == el) cnt1++;
-        }
-
-        if (cnt1 > (n / 2)) return el;
-        return -1;
+    return -1;
         
     }
 };
